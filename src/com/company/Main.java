62fac6a2e5dd2c73efc7bb01473ed7c5028956ff
Kateryna Borysova <katarina.bk@ukr.net>
2019-06-02 16:05:37 +0300

    fixed Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Student student1 = new Student("Kate", 12);
-	    Student student2 = new Student("Vasia", 5);
-        Student student3 = new Student("Lilia", 7);
+	    Student student1 = new Student("Kate");
+	    student1.setRating(12);
+	    Student student2 = new Student("Vasia");
+	    student2.setRating(5);
+        Student student3 = new Student("Lilia");
+        student3.setRating(7);
 
         Student[] students = new Student[] {student1, student2, student3};
         System.out.println(Student.getAverageRating(students));
-        student2.changeRating(10);
+        student1.changeRating(2);
         System.out.println(Student.getAverageRating(students));
     }
 }
